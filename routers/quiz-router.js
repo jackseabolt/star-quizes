82231e6a0a145fb640e89512d9c7b5953163e6cb
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-15 16:24:05 -0800

    title again

--- routers/quiz-router.js
+++ routers/quiz-router.js
@@ -113,7 +113,7 @@ router.get('/:quizTitle/question/:sessionId', (req, res) => {
                             current: current + 1, 
                             quizLength, 
                             score,  
-                            title: quiz.title 
+                            title: res.params.quizTitle
                         }); 
                     })
                 })
